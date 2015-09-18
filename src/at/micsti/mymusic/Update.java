002500687c3f8f6be2009604080a2dce9f060c3a
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-09-18 08:22:21 +0200

    Fixed timestamp month error on update activity

--- src/at/micsti/mymusic/Update.java
+++ src/at/micsti/mymusic/Update.java
@@ -189,7 +189,7 @@ public class Update extends Activity {
 		cal.setTimeInMillis(unix * 1000);
 		
 		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH);
+		int month = cal.get(Calendar.MONTH) + 1;
 		int day = cal.get(Calendar.DAY_OF_MONTH);
 		int hour = cal.get(Calendar.HOUR_OF_DAY);
 		int minute = cal.get(Calendar.MINUTE);
